8410d226d9cb54275a01da43db957662dd4726d8
LGL <1455567024@qq.com>
2021-10-04 21:19:53 +0800

    提交了一个文件

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @ProjectName: gitstudy
+ * @Package: PACKAGE_NAME
+ * @ClassName: HelloWorld
+ * @Author: a
+ * @Description:
+ * @Date: 2021/10/4 21:18
+ * @Version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("git你好！");
+    }
+}
